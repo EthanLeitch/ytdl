cd1a96fc9ad2c3b11fd3286adf305d419a1aebe8
Ethan <ethanjohnleitch@gmail.com>
2020-05-28 22:51:39 +1200

    :apple: I can't even describe why this error existed.
    I can't even describe why this error existed.

---
+++ downloader.py
@@ -0,0 +1,4 @@
+from pytube import YouTube
+import os
+
+video = YouTube("https://www.youtube.com/watch?v=wDgQdr8ZkTw")
--- pytube.py
+++
@@ -1,5 +0,0 @@
-from pytube import YouTube
-import os
-
-video = YouTube("https://www.youtube.com/watch?v=wDgQdr8ZkTw").streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first() 
-ytvideo.download("Downloads/")
