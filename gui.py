fc86137685f8166bea0094fe0743764d420b29ee
Alex <alexander.seaton@mcglashan.school.nz>
2020-05-28 17:35:03 +1200

    Gui now works

--- gui.py
+++ gui.py
@@ -1,11 +1,35 @@
-from PyQt5.QtWidgets import QApplication, QLabel
-from PyQt5 import QtWidgets
+import sys
+import os, curses, sys, subprocess
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QWidget, QLabel, QLineEdit
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtCore import QSize    
 
-app = QApplication([])
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
 
-label = QLabel('Enter a YouTube URL in the box below')
-label.show()
+        self.setMinimumSize(QSize(320, 140))    
+        self.setWindowTitle("YouTube Downloader") 
 
-urlbox = QtWidgets.QLineEdit()
-urlbox.show()
-app.exec_()
+        self.url = QLabel(self)
+        self.url.setText('URL:')
+        self.line = QLineEdit(self)
+
+        self.line.move(80, 20)
+        self.line.resize(200, 32)
+        self.url.move(20, 20)
+
+        pybutton = QPushButton('Download', self)
+        pybutton.clicked.connect(self.clickMethod)
+        pybutton.resize(200,32)
+        pybutton.move(80, 60)        
+
+    def clickMethod(self):
+        os.system("youtube-dl -f mp4 " + self.line.text())
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = MainWindow()
+    mainWin.show()
+    sys.exit( app.exec_() )
