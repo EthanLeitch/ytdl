c324dc233b0416b0d2024cc7b4ccfaf1ddca3d73
Ethan <ethanjohnleitch@gmail.com>
2020-05-29 13:43:40 +1200

    :sparkles: Added a proper time format
    Added a proper time format

--- downloader.py
+++ downloader.py
@@ -2,4 +2,12 @@ from pytube import YouTube
 
 yt = YouTube("https://www.youtube.com/watch?v=5jKZ9KGtee0")
 print("Title: " + str(yt.title))
-print(str(yt.length) + " seconds long")
+
+length = yt.length
+if length < 60:
+    print(str(length%60) + " seconds")
+else:
+    if length%60 < 10:
+        print(str(length//60) + ":" + "0" + str(length%60))
+    else:
+        print(str(length//60) + ":" + str(length%60))
